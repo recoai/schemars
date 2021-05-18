dc384953a25ba80f4c4a709064c19151b6311eaa
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2021-05-18 17:07:36 +0200

    Add description

--- schemars_derive/src/schema_exprs.rs
+++ schemars_derive/src/schema_exprs.rs
@@ -138,10 +138,7 @@ fn expr_for_external_tagged_enum<'a>(
     deny_unknown_fields: bool,
 ) -> TokenStream {
     let (unit_variants, complex_variants): (Vec<_>, Vec<_>) =
-        variants.partition(|v| v.is_unit() && v.attrs.with.is_none() && v.attrs.title.is_none());
-
-    // let unit_variants: Vec<&Variant> = vec![];
-    // let complex_variants : Vec<_> = variants.collect();
+        variants.partition(|v| v.is_unit() && v.attrs.with.is_none() && v.attrs.title.is_none() && v.attrs.description.is_none());
 
     let unit_names = unit_variants.iter().map(|v| v.name());
     let unit_schema = schema_object(quote! {
